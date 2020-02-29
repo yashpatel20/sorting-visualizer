ea078e6ac9368d8ab39d24b9fb9d8620c7b7ba26
Yashpatel20 <yash.work10@gmail.com>
2020-02-29 15:54:33 +0530

    completed quicksort animations

--- src/SortingAlgorithms/getQuickSortAnimations.js
+++ src/SortingAlgorithms/getQuickSortAnimations.js
@@ -1,33 +1,49 @@
+let animations = []
+
 export default function getQuickSortAnimations(arr){
-    const array = [...arr]
-    const animations = quickSort(array, 0, array.length-1)
+    let array = [...arr]
+    quickSort(array, 0, array.length-1,animations)
     console.log(animations)
     return animations
 }
 
-//TODO : understand how to reference an array in js
-
-function quickSort(array, left, right){
-    const arr = [...array]
-    //generate a random pivot
-    const pivot_idx = Math.floor( Math.random()*(right-left+1)+left )
-    console.log(pivot_idx)
-    //partition based on that random pivot
-    const new_pivot_idx = partition(left, right, arr, pivot_idx)
-    quickSort(arr, left, new_pivot_idx-1)
-    quickSort(arr, new_pivot_idx+1, right)
-    return arr
+let quickSort = (array, left, right)=>{
+    if(left < right){
+        let pivot_idx = Math.floor( Math.random()*(right-left+1)+left )
+        let new_pivot_idx = partition(left, right, array, pivot_idx,animations)
+        quickSort(array, left, new_pivot_idx-1)
+        quickSort(array, new_pivot_idx+1, right)
+    }
+    return array
 }
 
-function partition(left, right, array, pivot_idx){
-    const pivot_Val = array[pivot_idx]
-    //swap A[pivot_idx] with A{right}
-    [array[pivot_idx], array[right]] = [array[right],array[pivot_idx]]
+let partition = (left, right, array, pivot_idx)=>{
+    let pivot_Val = array[pivot_idx]
+    //swap randomized pivot and right
+    animations.push([pivot_idx,right,true])
+    animations.push([pivot_idx,right,false])
+    animations.push([pivot_idx,array[right],right,array[pivot_idx]])
+    swap(array,pivot_idx,right)
     let new_pivot_idx = left
     for(let i=left; i<right; i++){
-        if(array[i] < pivot_Val) [array[i],array[new_pivot_idx]] = [array[new_pivot_idx],array[i]]
-        new_pivot_idx++
+        //comparing
+        animations.push([i,right,true])
+        animations.push([i,right,false])
+        if(array[i] < pivot_Val){ 
+            //swap animation
+            animations.push([i,array[new_pivot_idx],new_pivot_idx,array[i]])
+            swap(array,i,new_pivot_idx++)
+        }else animations.push([i,array[i],new_pivot_idx,array[new_pivot_idx]])
     }
-    [array[new_pivot_idx],array[right]] = [array[right],array[new_pivot_idx]]
+    animations.push([new_pivot_idx,array[right],right, array[new_pivot_idx]])
+    swap(array,new_pivot_idx,right)
     return new_pivot_idx
 }
+
+let swap = (arr, a, b) => {
+    let temp = arr[a]
+    arr[a] = arr[b]
+    arr[b] = temp
+}
+
+
--- src/SortingVisualizer.js
+++ src/SortingVisualizer.js
@@ -5,16 +5,17 @@ import getBubbleSortAnimations from './SortingAlgorithms/getBubbleSortAnimations
 import getQuickSortAnimations from './SortingAlgorithms/getQuickSortAnimations'
 
 // Change this value for the speed of the animations.
-const ANIMATION_SPEED_MS = 0.1;
+const ANIMATION_SPEED_MS = 2
 
 // Change this value for the number of bars (value) in the array.
-const NUMBER_OF_ARRAY_BARS = 320;
+const NUMBER_OF_ARRAY_BARS = 400
+
 
 // This is the main color of the array bars.
-const PRIMARY_COLOR = 'turquoise';
+const PRIMARY_COLOR = 'turquoise'
 
 // This is the color of array bars that are being compared throughout the animations.
-const SECONDARY_COLOR = 'red';
+const SECONDARY_COLOR = 'red'
 
 function randomIntFromInterval(min,max){
     return Math.floor(Math.random()*(max-min+1)+min)
@@ -85,28 +86,71 @@ function SortingVisualizer(){
                   barOneStyle.height = `${newHeight}px`
                 }, i * ANIMATION_SPEED_MS);
               }
-
         }
     }
 
     const quickSort = () => {
-        getQuickSortAnimations([3,1,6,5,10,2])
+        const animations = getQuickSortAnimations(arr)
+        for(let i=0;i<animations.length;i++){
+            const arrayBars = document.getElementsByClassName('array-bar')
+            const isColorChange = (animations[i].length === 3 )
+            if(isColorChange){
+                const [barOneIdx,barTwoIdx] = animations[i]
+                const barOneStyle = arrayBars[barOneIdx].style
+                const barTwoStyle = arrayBars[barTwoIdx].style
+                const color = animations[i][2] ? SECONDARY_COLOR : PRIMARY_COLOR
+                setTimeout(() => {
+                    barOneStyle.backgroundColor = color;
+                    barTwoStyle.backgroundColor = color;
+                  }, i * ANIMATION_SPEED_MS);
+            }else{
+                setTimeout(() => {
+                    const bar = animations[i]
+                    const barOneStyle = arrayBars[bar[0]].style
+                    const barTwoStyle = arrayBars[bar[2]].style
+                    barOneStyle.height = `${bar[1]}px`
+                    barTwoStyle.height = `${bar[3]}px`
+                  }, i * ANIMATION_SPEED_MS);
+            }
+
+        }
     }
 
 
-    const arrVal = arr.map((value,idx) => <div style={{height : `${value}px`}} className="array-bar" key={idx}></div>)
+    const numWidth = Math.floor(window.screen.width / (arr.length * 3));
+    console.log("width: "+window.screen.width)
+    console.log("width: "+window.screen.height)
+    const width = `${numWidth}px`;
+    const numMargin = arr.length < 5 ?
+      10 : arr.length < 8 ?
+        8 : arr.length < 11 ?
+          6 : arr.length < 20 ?
+            4 : arr.length < 50 ?
+              3.5 : arr.length < 100 ?
+                3 : arr.length < 130 ?
+                  2.5 : 2;
+    const margin = `${numMargin}px`;
+
+    const arrVal = arr.map((value,idx) => <div style={{height: `${value}px`, width: width, marginLeft: margin, marginRigh: margin}} className="array-bar" key={idx}></div>)
     return (
+        <div>
         <div className="array-container">
             {arrVal}
-            <button onClick={()=>{
-                const randomArr = resetArray()
-                setArr(randomArr)
-                window.location.reload()
-            }}>Generate New Array</button>
-            <button onClick={mergeSort}>Merge Sort</button>
-            <button onClick={bubbleSort}>Bubble Sort</button>
-            <button onClick={quickSort}>Bubble Sort</button>
         </div>
+            <div className='buttons'>
+                <button className='generateArray' onClick={()=>{
+                    const randomArr = resetArray()
+                    setArr(randomArr)
+                    window.location.reload()
+                }}>Generate New Array</button>
+                <button className='sort' onClick={bubbleSort}>Bubble Sort</button>
+                <button className='sort' onClick={mergeSort}>Merge Sort</button>
+                <button className='sort' onClick={quickSort}>Quick Sort</button>
+                <button className='sort' onClick={quickSort}>Heap Sort</button>
+                <button className='sort' onClick={quickSort}>Counting Sort</button>
+                <button className='sort' onClick={quickSort}>Radix Sort</button>
+            </div>
+         </div>
     )
 }
 
