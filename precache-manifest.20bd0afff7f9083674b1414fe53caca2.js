dfdd29befe8b1f4dd6ef4085cbd5142e4775f8cc
Yashpatel20 <yash.work10@gmail.com>
2020-03-20 15:14:40 +0530

    Updates

---
+++ precache-manifest.20bd0afff7f9083674b1414fe53caca2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "101b2a688c1a516efeb76faf1bdc19d0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "410ccaac66d2f85d7870",
+    "url": "/static/css/main.91abd13d.chunk.css"
+  },
+  {
+    "revision": "b37f002db04741f2040c",
+    "url": "/static/js/2.4cd43288.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/static/js/2.4cd43288.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "410ccaac66d2f85d7870",
+    "url": "/static/js/main.da4f7d8d.chunk.js"
+  },
+  {
+    "revision": "1072cf085195b3c69add",
+    "url": "/static/js/runtime-main.814e0fff.js"
+  }
+]);
